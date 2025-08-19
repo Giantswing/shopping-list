21e77a812ffda44bb6daddeb9d1fc8388212ca8c
Alejandro <alejandropino88@gmail.com>
2025-08-19 17:43:17 +0200

    Update BasketList.vue to use product_id as key in BasketEntry component and simplify loading state check. This change improves the uniqueness of keys for rendering basket entries and enhances the loading state display logic.

--- src/views/BasketList.vue
+++ src/views/BasketList.vue
@@ -15,13 +15,10 @@ import BasketEntry from "./BasketEntry.vue";
       class="w-full flex flex-col-reverse gap-3 items-center p-3 pt-4"
       v-auto-animate="{ duration: 75 }"
     >
-      <BasketEntry v-for="entry in useBasket.basketProducts" :key="entry.id" :entry="entry" />
+      <BasketEntry v-for="entry in useBasket.basketProducts" :key="entry.product_id" :entry="entry" />
     </div>
 
-    <div
-      v-else-if="useBasket.loading.basketProducts && useBasket.basketProducts?.length === 0"
-      class="w-full flex flex-col items-center h-full justify-center"
-    >
+    <div v-else-if="useBasket.loading.basketProducts" class="w-full flex flex-col items-center h-full justify-center">
       <CIcon :icon="'line-md:loading-twotone-loop'" class="w-[100px] h-[100px] text-gray-500" />
     </div>
 
