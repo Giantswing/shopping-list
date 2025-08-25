31611f4ec871c4012b6ceaa61edb0c70db88da5b
Alejandro <alejandropino88@gmail.com>
2025-08-25 14:32:47 +0200

    Update basket app version to 1.1.0 for improved functionality and compatibility.

--- src/stores/basket.js
+++ src/stores/basket.js
@@ -10,7 +10,7 @@ const env = import.meta.env.VITE_APP_ENV;
 
 export const basket = defineStore("basket", {
   state: () => ({
-    basketAppVersion: '1.0.0',
+    basketAppVersion: '1.1.0',
     newProductInput: '',
     currentView: 'list',
     burguerMenuOpen: false,
